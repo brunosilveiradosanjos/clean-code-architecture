1b19a2e31d6d354ce2e1795d1b175b1ff8459e45
BrunoAnjos <brunosilveiradosanjos@gmail.com>
2021-08-10 01:11:33 -0300

    Aula 2 checkpoint

--- aula/src/Order.test.ts
+++ aula/src/Order.test.ts
@@ -1,3 +1,17 @@
-test("Não deve criar um pedido com o CPF inválido", function () {
-    expect(1).toBe(1);
+import Order from './Order';
+
+test('Não deve criar um pedido com o CPF inválido', function () {
+    // const cpf = '111.111.111-11';
+    const cpf = '39753740808';
+    expect(() => new Order(cpf)).toThrow(new Error('Invalid CPF'))
 });
+
+// test('Deve criar um pedido com 3 itens', function () {
+//     const cpf = '39753740808';
+//     const order = new Order(cpf);
+//     order.addItem('Guitarra', 1000, 1);
+//     order.addItem('Amplificador', 5000, 1);
+//     order.addItem('Cabo', 30, 2);
+//     const total = order.getTotal();
+//     expect(total).toBe(6600);
+// })
---
+++ aula/src/Order.ts
@@ -0,0 +1,44 @@
+export default class Order {
+    cpf: string;
+    constructor(cpf: string) {
+        // console.log(`this.validateCpf(${cpf})`, this.validateCpf(cpf));
+        if (this.validateCpf(cpf)) throw new Error("Invalid CPF");
+        this.cpf = cpf;
+    }
+
+    private CPF_DIVIDER = 11;
+    private DIGIT_SIZE_1 = 9;
+    private DIGIT_SIZE_2 = 10;
+
+    checkLengh(cpf: string) {
+        if (cpf == null || cpf == undefined) return false;
+        return cpf.toString().length === 11 ? true : false;
+    }
+
+    getOnlyDigits(cpf: string) {
+        return cpf.replace(/\D/g, '')
+    }
+
+    atLeastOneDifferentDigit(cpf: string) {
+        const [digit1] = cpf;
+        return !cpf.split("").every(digit => digit == digit1);
+    }
+
+    calculateDigit(cpf: string, index: number, size: number) {
+        let total = 0;
+        [...cpf.toString().substr(0, size)].forEach(element => {
+            total += parseInt(element) * index--;
+        });
+        const rest = (total) % this.CPF_DIVIDER
+        return rest < 2 ? 0 : this.CPF_DIVIDER - rest;
+    }
+
+    validateCpf(cpf: string) {
+        cpf = this.getOnlyDigits(cpf);
+        if (!this.checkLengh(cpf)) return false;
+        if (!this.atLeastOneDifferentDigit(cpf)) return false;
+        const digit = cpf.substr(-2, 2);
+        const calculatedDigit = `${this.calculateDigit(cpf, 10, this.DIGIT_SIZE_1)}${this.calculateDigit(cpf, 11, this.DIGIT_SIZE_2)}`
+        return digit == calculatedDigit;
+    }
+}
